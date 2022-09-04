6b08d3b06cf8f02c7bf79d82e0ed842e330df084
Abel <abelbrik@hotmail.com>
2022-09-05 01:08:31 +0200

    done

---
+++ ejercicio-2/index.js
@@ -0,0 +1,9 @@
+let book = {
+    title: "Guía del autoestopista galáctico",
+    author: "Douglas Adams",
+    year: 1978,
+    url: "https://www.amazon.es/Guia-autoestopista-galactico-Douglas-Adams-Libros/s?k=Guia+del+autoestopista+galactico+Douglas+Adams&rh=n%3A599364031"
+}
+
+const list = ["Abel", 30, true, new Date(1991, 10, 23), book];
+console.log(list)
