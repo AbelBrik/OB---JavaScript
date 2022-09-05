2b494fad6a4ef5ea969b6fb2d8d15179cb750d8b
Abel <abelbrik@hotmail.com>
2022-09-05 14:24:55 +0200

    done

--- ejercicio-4/index.js
+++ ejercicio-4/index.js
@@ -1,10 +1,10 @@
-let name = "Pepito";
-let surname = "Gargollo";
+let name = "Pe pito";
+let surname = "Garg ollo";
 let student = `${name} ${surname}`;
 let studentMayus = student.toUpperCase();
 let studentMinus = student.toLowerCase();
 let numbLetters = student.length
 let initialName = name[0];
 let lastSurname = surname[surname.length - 1];
-let withoutSpaces = student.replace(" ", "");
+let withoutSpaces = student.replace(/ /g, "");
 let contentName = student.includes(name);
---
+++ ejercicio-5/index.js
@@ -0,0 +1,6 @@
+const height = 170;
+const floatHeight = 1.7;
+const weight = 60.3;
+const heightCeil = Math.ceil(floatHeight);
+const weightFloor = Math.floor(weight);
+const maxNumber = Number.MAX_VALUE === Number.MAX_VALUE++;
